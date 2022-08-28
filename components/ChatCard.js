ae9fca14235d5a26db620525146a1a6b79cf70cc
unknown <mlab33972@gmail.com>
2022-08-28 03:24:20 -0400

    Almost Done - Lets go

--- components/ChatCard.js
+++ components/ChatCard.js
@@ -3,8 +3,8 @@ import Comment from "./assets/svg/comment"
 import Heart from "./assets/svg/heart"
 import MoreHorizontal from "./assets/svg/moreHorizontal"
 import Share from "./assets/svg/share"
-import BearishFilled from "./buttons/BearishFilled"
-import BullishFilled from "./buttons/bullishFilled"
+import BearishFilled from "./Buttons/BearishFilled"
+import BullishFilled from "./Buttons/BullishFilled"
 import Image from "next/image"
 
 const styles = {
