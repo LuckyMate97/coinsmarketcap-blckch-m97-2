ae02358cac12d58526d606d6ad7634efa567cf5f
unknown <mlab33972@gmail.com>
2022-08-28 03:16:03 -0400

    Almost Done - Lets go

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
    
   return (
     <MoralisProvider
-    children
+    
       serverUrl={process.env.NEXT_PUBLIC_SERVER!}
       appId={process.env.NEXT_PUBLIC_APP_ID!}
       >
